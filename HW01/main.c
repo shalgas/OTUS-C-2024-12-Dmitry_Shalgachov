69c6ef733d1ba1fc07ec5b3c8f8425592b7f2927
Dmitry Shalgachov <dmitry.shalgachov@gmail.com>
2025-01-06 01:32:03 +0300

    Recalling printf, scanf, argc/argv etc.

--- HW01/main.c
+++ HW01/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
  
-int main ()
+int main(int argc, char *argv[])
 {
-    printf("Hello World again!\n");
+   int a, b, sum;
+
+    printf("Please input a & b:\n");
+    scanf("%d %d", &a, &b);
+    printf("%d + %d = %d\n", a, b, a + b);
+
+    printf("Args == %d\n", argc -1 );
+    if (argc == 1) {
+        printf("Please, specify at least 1 Arg\n");
+        return(1);
+    } else {
+        printf("%d Args are:\n");
+        for (int i = 1; i <= argc; i++) {
+            printf("%s\n", argv[i]);
+        }
+    }
+
+    printf("\nHello World again!\n");
     return(0);
 }
